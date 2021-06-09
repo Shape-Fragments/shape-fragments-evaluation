ee9c24217935207317bac4892760c13080a7b8d3
Thomas Delva <thomas.delva@ugent.be>
2021-06-09 09:33:20 +0200

    add script for running all shapes and measuring times

---
+++ measure_times.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# create temporary folder where we will store our files:
+directory=/tmp/shape-views-validation 
+mkdir $directory
+mkdir $directory/tyrol
+
+for benchmark in "tyrol"; do
+  # download the data file for the benchmark if it does not exist yet
+  if [ ! -f $directory/$benchmark.nt ] ; then
+    curl -o $directory/$benchmark.nt "https://cloud.ilabt.imec.be/index.php/s/2HiMDZMQAsAKNgc/download?path=/&files=$benchmark.nt"
+  fi
+
+  echo "benchmark;shape;run;validation_time;extraction_time" > results.csv
+
+  for shape in $benchmark/*.ttl; do
+    for run in {1..5}; do
+      echo "###################"
+      echo "SHAPE / RUN :   $shape / $run"
+
+      # prioritize pySHACL over any other pyshacl import (e.g. from pip)
+      # expects pySHACL repository in sibling directory to this repository
+      export PYTHONPATH=../pySHACL
+
+      # expects pySHACL repository in sibling directory to this repository
+      validation_time=$(/usr/bin/python3.6 ../pySHACL/pyshacl/cli.py /tmp/shape-views-validation/$benchmark.nt -o=/tmp/shape-views-validation/$shape -s=$shape -df=nt -sf=turtle | head -n 1)
+
+      # sleep a bit to let the process close down completely
+      sleep 10
+    
+      # prioritize pyshacl-views over any other pyshacl import (e.g. from pip)
+      # expects pyshacl-views repository in sibling directory to this repository
+      export PYTHONPATH=../pyshacl-views
+
+      # expects pyshacl-views repository in sibling directory to this repository
+      extraction_time=$(/usr/bin/python3.6 ../pyshacl-views/pyshacl/cli.py /tmp/shape-views-validation/$benchmark.nt -o=/tmp/shape-views-validation/$shape -rs=True -s=$shape -df=nt -sf=turtle | head -n 1)
+
+      # sleep a bit to let the process close down completely
+      sleep 10
+
+      # append results to csv:
+      echo "$benchmark;$shape;$run;$validation_time;$extraction_time" >> results.csv
+    done
+  done
+done
