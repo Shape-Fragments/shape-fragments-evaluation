465f922155a6226cb1461c53ec513d04f62a20fe
Thomas Delva <thomas.delva@ugent.be>
2021-06-16 21:40:20 +0200

    add visualization script

--- visualize.py
+++ visualize.py
@@ -1,6 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
+from matplotlib.font_manager import FontProperties
 import numpy as np
 import statistics
 
@@ -33,24 +34,35 @@ with open('results.csv', 'r') as csvfile:
 # calculate avarage time between runs per shape for extraction/evaluation and also calculate overhead:
 eval_avarage = {}
 extract_avarage = {}
+eval_stdev = {}
+extract_stdev = {}
 overhead_average = {}
 for shape in eval_times:
     eval_avarage[shape] = sum(eval_times[shape]) / len(eval_times[shape])
+    eval_stdev[shape] = statistics.stdev(eval_times[shape])
+    extract_stdev[shape] = statistics.stdev(extract_times[shape])
     extract_avarage[shape] = sum(extract_times[shape]) / len(extract_times[shape])
     overhead_average[shape] = extract_avarage[shape] / eval_avarage[shape]
+print(f"Average evaluation time is {statistics.mean(eval_avarage.values())}\
+ seconds (stdev {statistics.stdev(eval_avarage.values())})")
 print(
-    f"Average evaluation time is {statistics.mean(eval_avarage.values())} seconds (stdev {statistics.stdev(eval_avarage.values())})")
-print(
-    f"Average extraction time is {statistics.mean(extract_avarage.values())} seconds (stdev {statistics.stdev(extract_avarage.values())})")
+    f"Average extraction time is {statistics.mean(extract_avarage.values())}\
+ seconds (stdev {statistics.stdev(extract_avarage.values())})")
+print(eval_stdev)
+print(extract_stdev)
 
 # get order index arrays for evaluation:
 increasing_eval_time = sorted(eval_avarage.keys(), key=eval_avarage.__getitem__)
 increasing_extract_time = sorted(extract_avarage.keys(), key=extract_avarage.__getitem__)
 increasing_overhead = sorted(overhead_average.keys(), key=overhead_average.__getitem__)
 print([eval_avarage[shape] for shape in increasing_eval_time])
+
+# latex font
+font = FontProperties(fname='OldStandardTT-Regular.ttf')
+
 # create plot
 fig, ax = plt.subplots()
-fig.set_size_inches(12.2/2.54, 8/2.54)
+fig.set_size_inches(12.2 / 2.54, 8 / 2.54)
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.spines['bottom'].set_visible(False)
@@ -70,27 +82,30 @@ rects2 = plt.bar(index + bar_width, [extract_avarage[shape] * 1000 for shape in 
                  color='grey',
                  label='Extraction')
 
-plt.xlabel('Shapes')
-plt.text(0.12, 0.96, "Time (ms)", horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+plt.xlabel('Shapes', fontproperties=font)
+plt.text(0.12, 0.96, "Time (ms)", horizontalalignment='center', verticalalignment='center', transform=ax.transAxes,
+         fontproperties=font)
 plt.yscale('log')
 plt.tick_params(
     axis='x',  # changes apply to the x-axis
     which='both',  # both major and minor ticks are affected
     bottom=False,  # ticks along the bottom edge are off
     top=False,  # ticks along the top edge are off
-    labelbottom=False)  # labels along the bottom edge are offplt.legend()
+    labelbottom=False),  # labels along the bottom edge are offplt.legend()
 plt.tick_params(
     axis='y',  # changes apply to the x-axis
     which='minor',  # minor ticks are affected
     left=False,  # ticks along the left edge are off
     right=False)  # ticks along the right edge are off
-plt.legend(loc='upper center', frameon=False)
+plt.legend(loc='upper center', frameon=False, prop=font)
 plt.tight_layout()
+for label in ax.get_yticklabels():
+    label.set_fontproperties(font)
 plt.show()
 fig.savefig("times.pdf")
 
 fig, ax = plt.subplots()
-fig.set_size_inches(12.2/2.54, 6/2.54)
+fig.set_size_inches(12.2 / 2.54, 6 / 2.54)
 plt.bar(range(len(increasing_overhead)),
         [(overhead_average[shape] - 1.0) * 100 for shape in increasing_overhead],
         color='grey')
@@ -100,9 +115,12 @@ ax.spines['bottom'].set_visible(False)
 ax.spines['left'].set_visible(False)
 plt.xticks([])
 plt.yticks([-10, 0, 10])
+for label in ax.get_yticklabels():
+    label.set_fontproperties(font)
 plt.text(0.25, 0.975, "Overhead (percentage)", horizontalalignment='center', verticalalignment='center',
-         transform=ax.transAxes)
-plt.text(0.5, 0.5, "Shapes", horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+         transform=ax.transAxes, fontproperties=font)
+plt.text(0.5, 0.5, "Shapes", horizontalalignment='center', verticalalignment='center', transform=ax.transAxes,
+         fontproperties=font)
 plt.tight_layout()
 plt.show()
 fig.savefig("overhead.pdf")
